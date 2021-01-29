c26019e0e2af99ad29a1897eaea726cd2117bf86
Dhaniel Diaz <ddiazaco@everis.com>
2021-01-29 12:58:27 -0500

    Mejoras en CSS y formulario

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const inputName = document.getElementById('inputName');
-const inputEmail = document.getElementById('inputEmail');
-const inputAge = document.getElementById('inputAge');
-const inputCountry = document.getElementById('inputCountry');
-const inputDni = document.getElementById('inputDni');
-const buttonPrint = document.getElementById('buttonPrint');
-const table = document.getElementById('table');
+const inputName = document.getElementById('inputName'),
+    inputEmail = document.getElementById('inputEmail'),
+    inputAge = document.getElementById('inputAge'),
+    inputCountry = document.getElementById('inputCountry'),
+    inputDni = document.getElementById('inputDni'),
+    buttonPrint = document.getElementById('buttonPrint'),
+    table = document.getElementById('table'),
+    form = document.querySelector("#form");
 
 const countrys = ['Venezuela','Colombia','Peru','Chile'];
 
@@ -15,7 +16,7 @@ for (let i = 0; i < countrys.length; i++) {
     inputCountry.appendChild(option);
 }
 
-buttonPrint.addEventListener('click',(e) => {
+form.addEventListener('submit',(e) => {
     e.preventDefault();
     const tr = document.createElement('tr');
     
@@ -23,31 +24,32 @@ buttonPrint.addEventListener('click',(e) => {
     thName.className= 'th';
     thName.textContent = inputName.value;
     tr.appendChild(thName);
-    inputName.value = '';
+    // inputName.value = '';
 
     const thEmail = document.createElement('th');
     thEmail.className = 'th';
     thEmail.textContent = inputEmail.value;
     tr.appendChild(thEmail);
-    inputEmail.value = '';
+    // inputEmail.value = '';
 
     const thAge = document.createElement('th');
     thAge.className = 'th';
     thAge.textContent = inputAge.value;
     tr.appendChild(thAge);
-    inputAge.value = '';
+    // inputAge.value = '';
 
     const thCountry = document.createElement('th');
     thCountry.className = 'th';
     thCountry.textContent = inputCountry.value;
     tr.appendChild(thCountry);
-    inputCountry.value = '';
+    // inputCountry.value = '';
 
     const thDni = document.createElement('th');
     thDni.className = 'th';
     thDni.textContent = inputDni.value;
     tr.appendChild(thDni);
-    inputDni.value = '';
+    // inputDni.value = '';
 
     table.appendChild(tr);
+    form.reset();
 });
