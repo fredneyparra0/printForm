e07fff457b6152a7b1f1debfd5d780d59d6c51ce
ney <fredneyparra0@gmail.com>
2021-01-28 23:04:13 -0400

    v01

---
+++ app.js
@@ -0,0 +1,53 @@
+const inputName = document.getElementById('inputName');
+const inputEmail = document.getElementById('inputEmail');
+const inputAge = document.getElementById('inputAge');
+const inputCountry = document.getElementById('inputCountry');
+const inputDni = document.getElementById('inputDni');
+const buttonPrint = document.getElementById('buttonPrint');
+const table = document.getElementById('table');
+
+const countrys = ['Venezuela','Colombia','Peru','Chile'];
+
+for (let i = 0; i < countrys.length; i++) {
+    const option = document.createElement('option');
+    option.textContent = countrys[i];
+    option.setAttribute('value',countrys[i]);
+    inputCountry.appendChild(option);
+}
+
+buttonPrint.addEventListener('submit',(e) => {
+    e.preventDefault();
+    const tr = document.createElement('tr');
+    
+    const thName = document.createElement('th');
+    thName.className= 'th';
+    thName.textContent = inputName.value;
+    tr.appendChild(thName);
+    inputName.value = '';
+
+    const thEmail = document.createElement('th');
+    thEmail.className = 'th';
+    thEmail.textContent = inputEmail.value;
+    tr.appendChild(thEmail);
+    inputEmail.value = '';
+
+    const thAge = document.createElement('th');
+    thAge.className = 'th';
+    thAge.textContent = inputAge.value;
+    tr.appendChild(thAge);
+    inputAge.value = '';
+
+    const thCountry = document.createElement('th');
+    thCountry.className = 'th';
+    thCountry.textContent = inputCountry.value;
+    tr.appendChild(thCountry);
+    inputCountry.value = '';
+
+    const thDni = document.createElement('th');
+    thDni.className = 'th';
+    thDni.textContent = inputDni.value;
+    tr.appendChild(thDni);
+    inputDni.value = '';
+
+    table.appendChild(tr);
+});
